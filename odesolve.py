8b8b1472bfe7a18df03f0e1dcc45f4b4a09dfd06
adampotton <98823308+adampotton@users.noreply.github.com>
2022-08-06 18:38:53 +0100

    Update odesolve.py

--- odesolve.py
+++ odesolve.py
@@ -31,7 +31,7 @@ def rk4(f, x, t, h):
 def solveto(f, x1, t1, t2, hmax, method=euler):
     while t1 < t2:
         x1 = euler(f, x1, t1, hmax)
-        t = t1 + hmax
+        t1 = t2 + hmax
         final = t2 - t1
     return euler(f, x1, t, final)
     pass
