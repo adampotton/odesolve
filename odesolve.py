ff2c751da5485cf53d57b87a7abf245c13cc5853
adampotton <98823308+adampotton@users.noreply.github.com>
2022-08-08 11:12:04 +0100

    Update odesolve.py

--- odesolve.py
+++ odesolve.py
@@ -31,26 +31,24 @@ def solveto(f, x1, t1, t2, hmax, method=euler):
     md = method
     if md == euler:
         while t1 <= t2:
-            xn = x1
             x1 = euler(f, x1, t1, hmax)
             t1 = t1 + hmax
             if t1 == t2:
                 return x1
             elif t1 > t2:
                 hmin = t2 - t1 + hmax
-                tn = t1 - hmax
-                return euler(f, xn, tn, hmin)
+                t1 = t1 - hmax
+                return euler(f, x1, t1, hmin)
     elif md == rk4:
         while t1 <= t2:
-            xn = x1
             x1 = rk4(f, x1, t1, hmax)
             t1 = t1 + hmax
             if t1 == t2:
                 return x1
             elif t1 > t2:
                 hmin = t2 - t1 + hmax
-                tn = t1 - hmax
-                return rk4(f, xn, tn, hmin)
+                t1 = t1 - hmax
+                return rk4(f, x1, t1, hmin)
     
 
 
