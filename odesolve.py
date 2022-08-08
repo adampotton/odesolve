5f173368cfb7af6b797661e65a094eb949b7675c
adampotton <98823308+adampotton@users.noreply.github.com>
2022-08-08 04:20:40 +0100

    Update odesolve.py

--- odesolve.py
+++ odesolve.py
@@ -28,7 +28,7 @@ def rk4(f, x, t, h):
 
 
 def solveto(f, x1, t1, t2, hmax, method=euler):
-    n = t2 - t1) / hmax
+    n = (t2 - t1) / hmax
     nfull = math.floor(n)
     hsmall = (t2 - t1) - (nfull * hmax)
     for i in range(n):
