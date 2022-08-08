2b68246113fc8b07fe553cd19d34d3efa01acc16
adampotton <98823308+adampotton@users.noreply.github.com>
2022-08-08 04:29:01 +0100

    Update odesolve.py

--- odesolve.py
+++ odesolve.py
@@ -32,9 +32,7 @@ def solveto(f, x1, t1, t2, hmax, method=euler):
     nfull = math.floor(n)
     hsmall = (t2 - t1) - (nfull * hmax)
     for i in range(nfull):
-        g = x1 
-        xn = x1 + hmax * g
-        x1 = xn
+        x1 = euler(f, x1, t1, hmax)
     if n % 1:
         return x1
     else:
